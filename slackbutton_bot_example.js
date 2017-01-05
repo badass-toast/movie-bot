b33bf94e5688c54e1a14786ce0d32debfeb1c3b1
Matteo Piatti <matteo.piatti@liip.xh>
2017-01-05 15:05:03 +0100

    Added working message match

--- slackbutton_bot_example.js
+++ slackbutton_bot_example.js
@@ -1,4 +1,4 @@
-var Botkit = require('../lib/Botkit.js');
+var Botkit = require('./node_modules/botkit/lib/Botkit.js');
 
 if (!process.env.clientId || !process.env.clientSecret || !process.env.port) {
   console.log('Error: Specify clientId clientSecret and port in environment');
@@ -58,83 +58,37 @@ controller.storage.teams.all(function(err,teams) {
 
 });
 
-var ask = 2;
 
 controller.hears(['movie (.*)'], ['ambient,message_received'], function(bot, message) {
-  var request = require("request");
-  var query = message.match[1];
-  var url = "https://api.themoviedb.org/3/search/movie?api_key=87a3acc12bd88c311e7dcc9c41542560&query=" +query+ "";
-  var base_url = "https://image.tmdb.org/t/p/w185";
+  var movie_search_title = message.match[1];
+  console.log(movie_search_title);
 
-  request({ url: url, json: true }, function (error, response, body) {
-    if(body.total_results != 0) {
-      var image_url = body.results[0].poster_path;
-      var movie_title = body.results[0].title;
-      var discription = body.results[0].overview;
-      var release_uncut = body.results[0].release_date;
-      var release = release_uncut.substring(0, 4);
-      var vote = body.results[0].vote_average;
-      var genre_id = body.results[0].genre_ids[0];
-      var genre = genre_find(genre_id);
-      if (!error && response.statusCode === 200) {
-        bot.reply(message, '///////////////////////////////////////////////////////////////////////////////////////////////////////////////');
-        bot.reply(message, '' + base_url + '' + image_url + '');
-        bot.reply(message, '*' + movie_title + '*');
-        bot.reply(message, '' + discription + '');
-        bot.reply(message, '_' + genre + ' ‧ ' + release + ' ‧ ' + vote + '/10_');
-        bot.reply(message, '///////////////////////////////////////////////////////////////////////////////////////////////////////////////');
-        bot.startConversation(message, function (err, convo) {
-          convo.ask({
-            attachments: [
-              {
-                title: 'Is this the right movie?',
-                callback_id: '123',
-                attachment_type: 'default',
-                actions: [
-                  {
-                    "name": "yes",
-                    "text": "Yes",
-                    "value": "yes",
-                    "type": "button",
-                  },
-                  {
-                    "name": "no",
-                    "text": "No",
-                    "value": "no",
-                    "type": "button",
-                  }
-                ]
-              }
-            ]
-          }, [
-            {
-              pattern: "yes",
-              callback: function (reply, convo) {
-                include
-                convo.next();
-              }
-            },
-            {
-              pattern: "no",
-              callback: function (reply, convo) {
-                var ask = 1;
-                convo.next();
-              }
-            },
-            {
-              default: true,
-              callback: function (reply, convo) {
-                // do nothing
-              }
-            }
-          ]);
-        });
-      }
+});
+
+/*var request = require("request");
+var query = message.match[1];
+var url = "https://api.themoviedb.org/3/search/movie?api_key=87a3acc12bd88c311e7dcc9c41542560&query=" +query+ "";
+var base_url = "https://image.tmdb.org/t/p/w185";
+
+request({ url: url, json: true }, function (error, response, body) {
+  if(body.total_results != 0) {
+    var image_url = body.results[0].poster_path;
+    var movie_title = body.results[0].title;
+    var discription = body.results[0].overview;
+    var release_uncut = body.results[0].release_date;
+    var release = release_uncut.substring(0, 4);
+    var vote = body.results[0].vote_average;
+    var genre_id = body.results[0].genre_ids[0];
+    var genre = genre_find(genre_id);
+    if (!error && response.statusCode === 200) {
+      bot.reply(message, '' + base_url + '' + image_url + '');
+      bot.reply(message, '*' + movie_title + '*');
+      bot.reply(message, '' + discription + '');
+      bot.reply(message, '_' + genre + ' ‧ ' + release + ' ‧ ' + vote + '/10_');
     } else if(body.total_results == 0){
       bot.reply(message, 'I wasn\'t able to find this movie:sweat: I am very sorry:cry:');
     }
-  });
-});
+  });*/
 
 
 
